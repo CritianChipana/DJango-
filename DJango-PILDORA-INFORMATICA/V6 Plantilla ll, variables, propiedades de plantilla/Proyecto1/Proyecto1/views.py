bbec4938f3fee94d733c2050c80519559a2dd2e2
CritianChipana <cristianchipanahuaman@gmail.com>
2020-12-22 18:00:38 -0500

    Mandar valores al html, usando Diccionarios

---
+++ DJango-PILDORA-INFORMATICA/V6 Plantilla ll, variables, propiedades de plantilla/Proyecto1/Proyecto1/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template,Context
+
+
+class Persona(object):
+    def __init__(self,nombre,apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
+
+def saludo(request):
+
+    p1=Persona("Carla","Ascues")
+
+    # nombre ="Cristian"
+    # apellido ="Chipana"
+    ahora = datetime.datetime.now()
+    
+    doc_externo =open("C:/Users/admi/Desktop/DJango-/DJango-PILDORA-INFORMATICA/V6 Plantilla ll, variables, propiedades de plantilla/Proyecto1/Proyecto1/plantillas/miplantilla.html")
+    
+    plt=Template(doc_externo.read())
+    doc_externo.close()
+    ctx=Context({"nombre_persona":p1.nombre,"apellido_persona":p1.apellido,"momento_actual":ahora})
+
+
+    documento = plt.render(ctx)
+
+    return HttpResponse(documento)
+
+
+def despedida(request):
+    return HttpResponse("adios mmgv")
+
+def damefecha(request):
+    fecha_actual = datetime.datetime.now()
+    mensaje= """
+                La fecha actual es %s
+                """%fecha_actual
+    return HttpResponse(mensaje)
+
+def calcular(request,ano,b):
+    b=18
+    # adadactual=18
+    perioda = ano-2020
+    edadfutura = b + perioda
+
+    documento = f"en el año '{ano}' tendras '{edadfutura}' años "
+    return HttpResponse(documento)
