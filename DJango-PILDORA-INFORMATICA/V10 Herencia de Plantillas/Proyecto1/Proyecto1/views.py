1cf2bfc15c42dd59d432ed45897bf8a88825db2d
CritianChipana <cristianchipanahuaman@gmail.com>
2020-12-24 18:23:12 -0500

    Herencia de Plantillas

---
+++ DJango-PILDORA-INFORMATICA/V10 Herencia de Plantillas/Proyecto1/Proyecto1/views.py
@@ -0,0 +1,56 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template,Context
+from django.template import loader
+from django.shortcuts import render
+
+
+class Persona(object):
+    def __init__(self,nombre,apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
+
+def saludo(request):
+
+    p1=Persona("Carla","Ascues")
+
+    temas_curso= ["plantillas","modelos","formularios","vistas","despliegue"]
+
+    ahora = datetime.datetime.now()
+
+    #doc_externo = loader.get_template('miplantilla.html'), 2017230428
+ 
+
+    ctx=({"nombre_persona":p1.nombre,"apellido_persona":p1.apellido,"momento_actual":ahora, "temas":temas_curso})
+    
+    # documento = doc_externo.render(ctx)
+    # return HttpResponse(documento)
+    return render(request,"miplantilla.html",ctx)
+
+def cursoC(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request,"CursoC.html",{"damefecha":fecha_actual})
+
+def cursoCSS(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request,"cursoCSS.html",{"damefecha":fecha_actual})
+
+def despedida(request):
+    return HttpResponse("adios mmgv")
+
+def damefecha(request):
+    fecha_actual = datetime.datetime.now()
+    mensaje= """
+                La fecha actual es %s
+                """%fecha_actual
+    return HttpResponse(mensaje)
+
+def calcular(request,ano,b):
+    b=18
+    # adadactual=18
+    perioda = ano-2020
+    edadfutura = b + perioda
+
+    documento = f"en el año '{ano}' tendras '{edadfutura}' años "
+    return HttpResponse(documento)
