f312a3a8fb9ee84411e08998c568c62c2769d7c9
CritianChipana <cristianchipanahuaman@gmail.com>
2020-12-24 11:37:56 -0500

    Cargadores de plantillas

---
+++ DJango-PILDORA-INFORMATICA/V8 Filtro y cargadores de plantilla/Proyecto1/Proyecto1/views.py
@@ -0,0 +1,54 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template,Context
+from django.template import loader
+
+
+class Persona(object):
+    def __init__(self,nombre,apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
+
+def saludo(request):
+
+    p1=Persona("Carla","Ascues")
+
+    # nombre ="Cristian"
+    # apellido ="Chipana"
+
+    temas_curso= ["plantillas","modelos","formularios","vistas","despliegue"]
+
+    ahora = datetime.datetime.now()
+  
+    # doc_externo =open("C:/Users/admi/Desktop/DJango-/DJango-PILDORA-INFORMATICA/V8 Filtro y cargadores de plantilla/Proyecto1/Proyecto1/plantillas/miplantilla.html")
+    # plt=Template(doc_externo.read())
+    # doc_externo.close()
+
+    doc_externo = loader.get_template('miplantilla.html')
+
+    ctx=({"nombre_persona":p1.nombre,"apellido_persona":p1.apellido,"momento_actual":ahora, "temas":temas_curso})
+
+    
+    documento = doc_externo.render(ctx)
+    return HttpResponse(documento)
+
+
+def despedida(request):
+    return HttpResponse("adios mmgv")
+
+def damefecha(request):
+    fecha_actual = datetime.datetime.now()
+    mensaje= """
+                La fecha actual es %s
+                """%fecha_actual
+    return HttpResponse(mensaje)
+
+def calcular(request,ano,b):
+    b=18
+    # adadactual=18
+    perioda = ano-2020
+    edadfutura = b + perioda
+
+    documento = f"en el año '{ano}' tendras '{edadfutura}' años "
+    return HttpResponse(documento)
