1bc423929d79f843f5a0a91e5af92e66e5f496c6
CritianChipana <cristianchipanahuaman@gmail.com>
2021-01-01 23:26:34 -0500

    V19 Panel de Administracion

--- DJango-PILDORA-INFORMATICA/V19 Panel de Administracion 4/tiendaonline/miapp/admin.py
+++ DJango-PILDORA-INFORMATICA/V19 Panel de Administracion 4/tiendaonline/miapp/admin.py
@@ -5,13 +5,21 @@ from miapp.models import Cliente,Articulo,Pedido
 # Register your models here.
 
 class ClientesAdmin(admin.ModelAdmin):
-    list_display=("nombre","direccion","tfno")
-    search_fields=("nombre","tfno")
+    list_display=("nombre","direccion","tfno") # transforma los objetos a informacion en formato tabla
+    search_fields=("nombre","tfno") #Crea un textFiel para buscar con los campos llamados
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter=("seccion",)
 
 
-admin.site.register(Cliente,ClientesAdmin)
-admin.site.register(Articulo)
-admin.site.register(Pedido)
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=("numero","fecha")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+admin.site.register(Cliente,ClientesAdmin) #Si se crea listas 
+admin.site.register(Articulo,ArticulosAdmin)
+admin.site.register(Pedido,PedidosAdmin)
 
 
 
